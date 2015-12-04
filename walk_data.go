2be05cd576c0d4c67253247acf69235e76cf8a32
Viktor Kojouharov <developer@sugr.org>
2015-12-04 22:17:52 +0200

    supply the connector names and the parent node in the walk data

--- walk_data.go
+++ walk_data.go
@@ -1,14 +1,27 @@
 package graph
 
 type WalkData struct {
-	Node       Node
-	Connectors []Connector
+	Node    Node
+	Parents []Parent
 
 	done chan struct{}
 }
 
-func NewWalkData(n Node, c []Connector, d chan struct{}) WalkData {
-	return WalkData{Node: n, Connectors: c, done: d}
+type Parent struct {
+	From string
+	To   string
+	Node Node
+}
+
+func NewWalkData(n Node, conns []Connector, d chan struct{}) WalkData {
+	parents := []Parent{}
+	for _, c := range conns {
+		if t, o := c.Target(); t != nil {
+			parents = append(parents,
+				Parent{From: o.Name(), To: c.Name(), Node: t.Node()})
+		}
+	}
+	return WalkData{Node: n, Parents: parents, done: d}
 }
 
 func (wd WalkData) Close() {
